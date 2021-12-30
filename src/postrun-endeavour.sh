2982a062c2809203a2368e6404dac9291e628277
K <terry.a.davis@yandex.com>
2021-12-30 21:28:03 +0900

    Update postrun-endeavour.sh

--- src/postrun-endeavour.sh
+++ src/postrun-endeavour.sh
@@ -1,1 +1,6 @@
+# KDE Plasma theme switch to default (user mode)
+if pacman -Qq | grep -q plasma-desktop; then
+	/usr/lib/plasma-changeicons breeze-dark 2>/dev/null
+	lookandfeeltool --apply "org.kde.breezedark.desktop" 2>/dev/null
+fi
 sed -i '/endeavouros-i3wm-setup/d' ~/.config/i3/config
