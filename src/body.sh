35740174237b6d8cb7ab0d121da162a29d197e00
K <terry.a.davis@yandex.com>
2021-12-30 23:26:44 +0900

    Update body.sh

--- src/body.sh
+++ src/body.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
 # Inspired by mariuszkurek/convert.sh
 
+
+#no. no manjaro. manjaro bad. convert fast. right now.
+CONVERTER_MANJARNO(){
+## System-wide jobs. The core part, this breaks and you system gets borked.
+## If you use doas instead of sudo, then simply change the sudo to doas.
+sudo bash -c '
+__CONVERTSCRIPT_MANJARNO__
+' 2>/dev/null # 2>/dev/null is for error redirection.
+
+## User-wide jobs. Some important cleanup jobs, especially for sway. Makes script more seamless.
+__POSTSCRIPT_MANJARNO__
+}
+
+#endeavour better. still not arch. let's convert.
+CONVERTER_ENDEAVOUROS(){
+## System-wide jobs. The core part, this breaks and you system gets borked.
+## If you use doas instead of sudo, then simply change the sudo to doas.
+sudo bash -c '
+__CONVERTSCRIPT_ENDEAVOUROS__
+' 2>/dev/null # 2>/dev/null is for error redirection.
+
+## User-wide jobs. Some important cleanup jobs, especially for sway. Makes script more seamless.
+__POSTSCRIPT_ENDEAVOUROS__
+}
+
 # echo $EUID is also possible, but that method is useless in sudo.
 if [ "$(id -u)" == "0" ]; then
 	printf "This script should not be run as root.\nPermissions will be elevated automatically for system-wide tasks.\n"
@@ -10,38 +35,22 @@ fi
 [ $? == 1 ] && exit 1;
 
 
-prescript(){
-__PRESCRIPT__
-}
 
-#no. no manjaro. manjaro bad. convert fast. right now.
-CONVERTER_MANJARNO(){
-## System-wide jobs. The core part, this breaks and you system gets borked.
-## If you use doas instead of sudo, then simply change the sudo to doas.
-sudo bash -c '
-__CONVERTSCRIPT_MANJARNO__
-' 2>/dev/null # 2>/dev/null is for error redirection.
-## User-wide jobs. Some important cleanup jobs, especially for sway. Makes script more seamless.
-__POSTSCRIPT_MANJARNO__
-}
-
-#endeavour better. still not arch. let's convert.
-CONVERTER_ENDEAVOUR(){
-## System-wide jobs. The core part, this breaks and you system gets borked.
-## If you use doas instead of sudo, then simply change the sudo to doas.
-sudo bash -c '
-__CONVERTSCRIPT_ENDEAVOUR__
-' 2>/dev/null # 2>/dev/null is for error redirection.
-## User-wide jobs. Some important cleanup jobs, especially for sway. Makes script more seamless.
-__POSTSCRIPT_ENDEAVOUR__
-}
-
-DISTRO=$(prescript)
+DISTRO=$(
+printf "This script comes with ABSOLUTELY NO WARRANTY!\n" 1>&2
+printf "What distro? Press 1 if you run Manjaro, 2 if you run EndeavourOS.\n" 1>&2
+read -rn 1 whatdistro
+case "$whatdistro" in
+        "1") echo MANJARNO ;;
+        *) echo ENDEAVOUROS ;;
+esac
+read -rp "==>Press Enter to continue" 1>&2
+)
 
 if [ "${DISTRO}" == "MANJARNO" ]; then
     CONVERTER_MANJARNO
 else
-    CONVERTER_ENDEAVOUR
+    CONVERTER_ENDEAVOUROS
 fi
 
 
