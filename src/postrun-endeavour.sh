085e7147ae49533a4e4721f86fa86f14c23b1296
K <terry.a.davis@yandex.com>
2022-01-13 10:38:40 +0900

    Update postrun-endeavour.sh

--- src/postrun-endeavour.sh
+++ src/postrun-endeavour.sh
@@ -3,7 +3,7 @@ if pacman -Qq | grep -q plasma-desktop; then
 	/usr/lib/plasma-changeicons breeze-dark 2>/dev/null
 	lookandfeeltool --apply "org.kde.breezedark.desktop" 2>/dev/null
 fi
-[ "$(pacman -Qq | grep -q i3)" ] && sed -i '/endeavouros-i3wm-setup/d' ~/.config/i3/config
+[ -f $HOME/.config/i3/config ] && sed -i '/endeavouros-i3wm-setup/d' ~/.config/i3/config
 printf "Would you like to reboot? (y/N)"
 read -r reboot
 #Thanks to YTG1234 for this line.
