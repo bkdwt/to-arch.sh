3aeff1c341b6e4c95fd033adff1f50a817b6e045
Кейсуке Игараси <terry.a.davis@yandex.com>
2022-02-01 00:58:56 +0000

    Update postrun-garuda.sh

--- src/postrun-garuda.sh
+++ src/postrun-garuda.sh
@@ -1,7 +1,7 @@
 # KDE Plasma theme switch to default (user mode)
 if pacman -Qq | grep -q plasma-desktop; then
 	/usr/lib/plasma-changeicons breeze-dark 2>/dev/null
-	lookandfeeltool --apply "org.kde.breezedark.desktop" 2>/dev/null
+	lookandfeeltool --apply --resetLayout "org.kde.breezedark.desktop" 2>/dev/null
 fi
 
 # Deletes sway config cuz sway edition saves its configs in a packages and sway gets borked after deleteing those.
