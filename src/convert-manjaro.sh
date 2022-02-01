4d770c736515dd0bf1c7250d7d41210635be9265
Кейсуке Игараси <terry.a.davis@yandex.com>
2022-02-01 00:26:11 +0000

    Update convert-manjaro.sh

--- src/convert-manjaro.sh
+++ src/convert-manjaro.sh
@@ -18,11 +18,11 @@ printf "This is your current distro state.\n"
 
 if grepPacmanQuery pamac; then
 	# Pamac is there for a reason, and I'm not hostile towards it.
-	printf "\nDo you want to remove pamac?(y/N)\n"
+	printf "\nDo you want to remove pamac?(Y/n)\n"
 	read -rn 1 a
 	case "$a" in
-		[Yy]*) pacman -Qq | grep pamac | xargs pacman -Rdd --noconfirm ;;
-		*) printf "Leaving pamac alone.\n" ;;
+		[Nn]*) printf "Leaving pamac alone.\n" ;;
+		*) pacman -Qq | grep pamac | xargs pacman -Rdd --noconfirm ;;
 	esac
 fi
 grepPacmanQuery manjaro-application-utility && pacman -Rcnsdd manjaro-application-utility --noconfirm
