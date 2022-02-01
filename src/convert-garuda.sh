c7440bad454bc62092f44bc316272e547c1d1459
Кейсуке Игараси <terry.a.davis@yandex.com>
2022-02-01 00:25:25 +0000

    Update convert-garuda.sh

--- src/convert-garuda.sh
+++ src/convert-garuda.sh
@@ -16,20 +16,18 @@ pacman -Syy neofetch micro vim --noconfirm
 neofetch
 printf "This is your current distro state.\n"
 
+sudo pacman -R garuda-hooks
+
 if grepPacmanQuery pamac; then
 	# Pamac is there for a reason, and I'm not hostile towards it.
-	printf "\nDo you want to remove pamac?(y/N)\n"
+	printf "\nDo you want to remove pamac?(Y/n)\n"
 	read -rn 1 a
 	case "$a" in
-		[Yy]*) pacman -Qq | grep pamac | xargs pacman -Rdd --noconfirm ;;
-		*) printf "Leaving pamac alone.\n" ;;
+		[Nn]*) printf "Leaving pamac alone.\n" ;;
+		*) pacman -Qq | grep pamac | xargs pacman -Rdd --noconfirm ;;
 	esac
 fi
-grepPacmanQuery manjaro-application-utility && pacman -Rcnsdd manjaro-application-utility --noconfirm
-removeIfMatched matray
-removeIfMatched manjaro-release
-removeIfMatched bashrc-manjaro
-removeIfMatched manjaro-keyring
+
 
 
 (
@@ -62,11 +60,6 @@ removeIfMatched manjaro-keyring
 	cp /etc/pacman.d/mirrorlist "${tmp_dir}/mirrorlist"
 )
 
-# I have seen pacui and bmenu in some editions as a dependency
-grepPacmanQuery pacui && pacman -Qq | grep pacui | xargs pacman -Rdd --noconfirm
-pacman -Qq pacui &>/dev/null && pacman -Qq | grep pacui | xargs pacman -Rdd --noconfirm
-pacman -Qq bmenu &>/dev/null && pacman -Qq | grep bmenu | xargs pacman -Rdd --noconfirm
-
 # Manjaro uses a different mirrorlist package to identify from the Arch one.
 pacman -Qq pacman-mirrors &>/dev/null && pacman -Qq | grep pacman-mirrors | xargs pacman -Rdd --noconfirm
 
@@ -77,19 +70,8 @@ pacman -U --overwrite \*  https://www.archlinux.org/packages/core/x86_64/pacman/
 # Do it again, because conf gets reset
 sed -i '/SyncFirst/d' /etc/pacman.conf
 
-# # Deletes the Manjaro UEFI entry. It's a very dangerous operation if misused, but I tested this multiple times and it was good.
-#if [ -d /sys/firmware/efi ]; then
-	#efibootmgr>/tmp/efi_count_tmp
-	#count="$(grep -ic Manjaro /tmp/efi_count_tmp)"
-	#if (( count > 1 )); then
-		#continue
-	#else
-		#efibootmgr -b "$(efibootmgr | grep Manjaro | sed 's/*//' | cut -f 1 -d' ' | cut -c5-)" -B
-	#fi
-#fi
-
 # Change grub
-sed -i '/GRUB_DISTRIBUTOR="Manjaro"/c\GRUB_DISTRIBUTOR="Arch"' /etc/default/grub
+sed -i '/GRUB_DISTRIBUTOR="Garuda"/c\GRUB_DISTRIBUTOR="Arch"' /etc/default/grub
 
 # Following line enables multilib repository
 sed -ie 's/#\(\[multilib\]\)/\1/;/\[multilib\]/,/^$/{//!s/^#//;}' /etc/pacman.conf
@@ -98,17 +80,11 @@ sed -ie 's/#\(\[multilib\]\)/\1/;/\[multilib\]/,/^$/{//!s/^#//;}' /etc/pacman.co
 sed -i '/HoldPkg/d' /etc/pacman.conf
 
 # Purge Manjaro's software
-pacman -Qq | grep manjaro | xargs pacman -Rdd --noconfirm
+pacman -Qq | grep garuda | xargs pacman -Rdd --noconfirm
 # KDE Plasma
-pacman -Qq | grep breath | xargs pacman -Rdd --noconfirm
+pacman -Qq | grep sweet | xargs pacman -Rdd --noconfirm
 
-# This should be in front of -Syu to avoid manjaro's linux kernel from updating
-pacman -Qq | grep 'linux[0-9]' | xargs pacman -Rdd --noconfirm
 
-#Manjaro SwayWM edition
-if [ "$(cat /etc/pacman.conf | grep '\[manjaro-sway\]')" ]; then
-	sudo sed -ie '/\[manjaro-sway\]/,+2d' /etc/pacman.conf
-fi
 ###################
 #TODO:Probably some kind of custom hook in manjarno.
 #/usr/share/libalpm/hooks/*
@@ -122,12 +98,12 @@ pacman -Qq | grep mhwd | xargs pacman -Rdd --noconfirm 2>/dev/null
 
 # Change computer's name if it's manjaro
 if [ -f /etc/hostname ]; then
-	sed -i '/manjaro/c\Arch' /etc/hostname
-	sed -i '/Manjaro/c\Arch' /etc/hostname
+	sed -i '/garuda/c\Arch' /etc/hostname
+	sed -i '/Garuda/c\Arch' /etc/hostname
 fi
 
-sed -i '/manjaro/c\Arch' /etc/hosts
-sed -i '/Manjaro/c\Arch' /etc/hosts
+sed -i '/garuda/c\Arch' /etc/hosts
+sed -i '/Garuda/c\Arch' /etc/hosts
 
 # linux-lts is generally more stable(especially for Intel graphics, uhd620 seems to have a black screen issue since 5.11)
 printf "What kernel? Press 1 for linux-lts(more stable), 2 for normal linux.\n"
@@ -143,9 +119,6 @@ if [ "$(lspci | grep -i nvidia)" ]; then
     pacman -S nvidia-dkms --noconfirm
 fi
 
-# Some wallpaper removal. I heard that it's in budgie and xfce editions.
-grepPacmanQuery illyria-wallpaper && pacman -Rdd illyria-wallpaper --noconfirm
-
 # Delete line that hides GRUB. Manjaro devs, do you think that noobs don't even know how to press enter?
 sed -i '/GRUB_TIMEOUT_STYLE=hidden/d' /etc/default/grub
 
@@ -155,7 +128,7 @@ if ! [ "$(bootctl is-installed | grep -i yes)" ]; then
 	[ -d /boot/grub/themes/archlinux ] && rm -rf /boot/grub/themes/archlinux
 	mkdir /boot/grub/themes/archlinux
 	tar -xf /tmp/arch.tar -C /boot/grub/themes/archlinux
-	sed -i '/GRUB_THEME=/c GRUB_THEME="/boot/grub/themes/archlinux/theme.txt"' /etc/default/grub
+	sed -i '/GRUB_THEME=.*/c GRUB_THEME="/boot/grub/themes/archlinux/theme.txt"' /etc/default/grub
 	# Generate GRUB stuff
 	[ -f /boot/grub/grub.cfg ] && rm /boot/grub/grub.cfg
 	[ -f /boot/grub/grub.cfg.new ] && rm /boot/grub/grub.cfg.new
@@ -163,7 +136,7 @@ if ! [ "$(bootctl is-installed | grep -i yes)" ]; then
 
 else 
     bootctl update
-    printf "Systemd-boot users have to edit the updated the entries manually.\nYou're on your own."
+    printf "Systemd-boot users have to edit the updated the entries manually.\nYou're on your own, good luck, my friend."
 fi
 # Locale fix
 # It scared the daylights out of me when I realized gnome-terminal won't start without this part
@@ -185,15 +158,15 @@ fi
 
 # I know... sorry...
 if [ -f /etc/os-release ]; then
-	sed -i 's/Manjaro/Arch/g' /etc/os-release
-	sed -i 's/ID=manjaro/ID=arch/g' /etc/os-release
-	sed -i 's/ANSI_COLOR="32;1;24;144;200"/ANSI_COLOR="38;2;23;147;209"/g' /etc/os-release
-	sed -i 's/manjaro/archlinux/g' /etc/os-release
-	sed -i 's/manjarolinuxlinux/archlinux/g' /etc/os-release
-	sed -i 's/archlinuxlinux/archlinux-logo/g' /etc/os-release
+	sed -i 's/Garuda/Arch/g' /etc/os-release
+	sed -i 's/ID=garuda/ID=arch/g' /etc/os-release
+	sed -i 's/SUPPORT_URL=\"https:\/\/forum.garudalinux.org\"/SUPPORT_URL=\"https:\/\/bbs.archlinux.org\"/g' /etc/os-release
+	sed -i 's/garudalinux/archlinux/g' /etc/os-release
+	sed -i '/BUG_REPORT_URL/d' /etc/os-release
+	sed -i 's/LOGO=garudalinux/LOGO=archlinux-logo/g' /etc/os-release
 fi
 
-[ -f /etc/issue ] && sed -i 's/Manjaro/Arch/g' /etc/issue
+[ -f /etc/issue ] && sed -i 's/Garuda/Arch/g' /etc/issue
 
 # Screenfetch takes an eternity to run in VMs. I have no damn idea why.
 neofetch
@@ -229,10 +202,10 @@ fi
 # This file is known to exist in the gnome edition, but somehow vanishes after reboot.
 # Still let's change it.
 if [ -f /etc/arch-release ]; then
-	sed -i '/Manjaro/c\Arch' /etc/arch-release
+	sed -i '/Garuda/c\Arch' /etc/arch-release
 fi
 
 
 
-[ -f /.manjaro-tools ] && rm -f /.manjaro-tools
+[ -f /.garuda-tools ] && rm -f /.manjaro-tools
 [ -d /var/lib/pacman-mirrors ] && rm -rf /var/lib/pacman-mirrors
