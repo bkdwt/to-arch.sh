f40538d182bfa30e03136ed0b16f13ca4e58404c
Кейсуке Игараси <terry.a.davis@yandex.com>
2022-02-01 00:59:22 +0000

    Update convert-manjaro.sh

--- src/convert-manjaro.sh
+++ src/convert-manjaro.sh
@@ -117,7 +117,7 @@ fi
 [ -f /etc/lsb-release ] && mv /etc/lsb-release /etc/lsb-release.bak
 pacman -Syyu --overwrite \* bash lsb-release --noconfirm
 
-# As Linus Torvalds said
+# As Linus Torvalds said, nvidia, fück you
 pacman -Qq | grep mhwd | xargs pacman -Rdd --noconfirm 2>/dev/null
 
 # Change computer's name if it's manjaro
